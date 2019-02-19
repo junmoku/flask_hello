7e14d3f447225d6f7ab0d6ef53cbd051b536998f
junmoku <junmoku@netmarble.com>
2019-02-19 16:13:16 +0900

    -

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+class chat():
+  def __init__(self):
+    self.chatContent = ""
+
+  def addContent(self, content):
+    self.chatContent += content
+
+  def getContent(self):
+    return self.chatContent
+
+c = chat()
 
 @app.route('/hello/<name>')
 def hello_world(name):
@@ -13,6 +25,11 @@ def hello_world_template():
 def hello_world_pull():
   return 'pull'
 
+@app.route('/chat', methods=['POST'])
+def chat():
+  c.addContent(request.get_data().decode('UTF-8'))
+  return c.getContent()
+
 if __name__ == '__main__':
-  app.run(port='5000', debug='true')
-  # app.run(host='0.0.0.0', port='5000')
+  # app.run(port='5000', debug='true')
+  app.run(host='0.0.0.0', port='5000', debug='true')
